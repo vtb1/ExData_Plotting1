94081e9b29593264a11f8846551c342c53f87fb4
Victor Torcida <victor.torcida@gmail.com>
2015-10-11 12:59:05 +0200

    upload new plot1.R file

--- plot1.R
+++ plot1.R
@@ -2,23 +2,21 @@
 #Load the file 
 #
 
-# Leemos el fichero
+# Read File
 library("dplyr")
 X <- read.table("./Curso4_p1/household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE)
 
-# Formateamos
+# Format Date
 X$V1 <- strptime (X$V1,format="%d/%m/%Y")
-X$V2 <- strptime (X$V2,format = "%H:%M:%S")
 
-#Modificamos el formato de los campos
+
+#Change to Data
 X$V1 <- as.Date(X$V1)
-#X$V2 <- as.Date(X$V2)
-X$V3 <- as.numeric(X$V3)
 
-# Filtramos por el día 2007-02-01 y 2007-01-01
+# Filtering data
 Y <- X[X$V1 %in% c('1/2/2007', '2/2/2007'),]
 
-# Cambiamos a numérico el global active power
+# Change global active power to numeric
 Y$V3 <- as.numeric(Y$V3)
 
 #pLOT 1
